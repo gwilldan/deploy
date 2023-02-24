4167b5e7c81baa3019ba7de8a650633b20c99300
gwilldan <sayhi2godswill@gmail.com>
2023-02-24 22:22:29 +0100

    first commit

---
+++ src/compIndex.js
@@ -0,0 +1,3 @@
+export {default as Header} from "./component/Header";
+export {default as Body} from "./component/Body";
+export {default as Footer} from "./component/Footer";
---
+++ src/component/Body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./body.css"
+
+function Body(props) {
+    return (
+        <div className='body'>
+            BODY
+        </div>
+    );
+}
+
+export default Body;
