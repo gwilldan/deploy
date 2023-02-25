6cd3f0e1c443029cd3a867124d31b4b5154caf2a
gwilldan <sayhi2godswill@gmail.com>
2023-02-26 00:16:55 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import {Header, Body, Footer} from "./compIndex";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Body />
-      <Footer/>
+      <Router>
+        <div className='Header'>
+            <Link to="">Header</Link>
+            <Link to="/Body">Body</Link>
+            <Link to="/Contact">Contact Us</Link>
+            <Link to="/Okon">Blog</Link>
+        </div>
+        <Routes >
+          <Route path='/' element={<Header />}/>
+          <Route path='/Body' element={<Body />}/>
+          <Route path='*' element={<h1>404 Not Found!</h1>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
